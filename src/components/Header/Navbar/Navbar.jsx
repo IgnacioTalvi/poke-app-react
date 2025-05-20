a946b3efac5130a881c73db187c09460257b409a
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-20 17:14:50 +0200

    centered content with scss

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
 
 const Nav = () => {
   return (
-    <nav className="nav-container">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/new">New Pokemon</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="nav-container">
+      <nav >
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/new">New Pokemon</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
--- src/components/Main/Home/SearchContainer/PokemonList/PokemonCard/PokemonCard.jsx
+++ src/components/Main/Home/SearchContainer/PokemonList/PokemonCard/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const PokemonCard = ({ pokemon }) => {
   const {sprites, name, height, weight} = pokemon;
   console.log(pokemon);
